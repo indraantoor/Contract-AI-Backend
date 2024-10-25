8ca9a6d3090cf390cbea6ec54b0cbd3d3a3700bb
indraantoor <indraantoor@gmail.com>
2024-10-25 17:16:59 +0530

    feat: authenticate and save user in db
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,16 @@ dotenv.config();
 
 import cors from "cors";
 import express from "express";
+import session from "express-session";
 import helmet from "helmet";
 import mongoose from "mongoose";
 import morgan from "morgan";
+import passport from "passport";
+import "./config/passport";
+
+// routes
+import MongoStore from "connect-mongo";
+import authRoute from "./routes/auth";
 
 const app = express();
 
@@ -20,6 +27,27 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI!,
+    }),
+    cookie: {
+      secure: process.env.NODE_ENV == "production",
+      sameSite: process.env.NODE_ENV == "production" ? "none" : "lax",
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", authRoute);
 
 const PORT = 8080;
 app.listen(PORT, () => {
---
+++ src/routes/auth.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import passport from "passport";
+
+const router = express.Router();
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    res.redirect(`${process.env.CLIENT_URL}/dashboard`);
+  }
+);
+
+router.get("/current-user", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json(req.user);
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+});
+
+export default router;
